ae6a6c777809cd81feb4aeb59290e3d95239f2d7
milouwicli <milou.wimart@nium.com>
2026-05-14 12:10:31 +0100

    Create monday.js

---
+++ api/monday.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
+  const response = await fetch('https://api.monday.com/v2', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': process.env.MONDAY_TOKEN,
+    },
+    body: JSON.stringify({ query: req.body.query }),
+  });
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
